78f7e0c66cc3d1c3ed3f268fb8a169a4fae95168
Alexandr Golobokov <bullet93639@mail.ru>
2023-11-18 02:58:26 +0300

    Done task 1.7_3

---
+++ white/week_1/task_1.7_3.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    int a, b;
+    
+    cin >> a >> b;
+    for (int x = a; x <= b; x++) {
+        if (x % 2 == 0) {
+            cout << x << " ";
+        }
+    }
+
+    return 0;
+}
