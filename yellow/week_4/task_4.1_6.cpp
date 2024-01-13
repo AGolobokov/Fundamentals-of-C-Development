1d9ee016fd708775aff13577ea5f1a87af25b814
Alexandr Golobokov <bullet93639@mail.ru>
2024-01-13 22:53:36 +0300

    Yellow week4 done task: chapter 4.1 step 6

---
+++ yellow/week_4/task_4.1_6.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+template <typename T>
+vector<T> FindGreaterElements(const set<T>& elements, const T& border) {
+    set<T> tmp;
+    for (const auto element : elements) {
+        if (element > border) {
+            tmp.insert(element);
+        }
+    }
+    vector<T> vc(tmp.begin(), tmp.end());
+    return vc;
+}
+
+
+// int main() {
+//   for (int x : FindGreaterElements(set<int>{1, 5, 7, 8}, 5)) {
+//     cout << x << " ";
+//   }
+//   cout << endl;
+  
+//   string to_find = "Python";
+//   cout << FindGreaterElements(set<string>{"C", "C++"}, to_find).size() << endl;
+//   return 0;
+// }
