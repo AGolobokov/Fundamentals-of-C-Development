f8932a4122bbb2e205d881f0cee4da3669e1454c
Alexandr Golobokov <bullet93639@mail.ru>
2024-02-03 18:34:43 +0300

    Yellow week2 done task: chapter 2.4 step 2

---
+++ yellow/week_2/task_2.4_2.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+ostream& operator << (ostream& os, const vector<T>& s) {
+  os << "{";
+  bool first = true;
+  for (const auto& x : s) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << x;
+  }
+  return os << "}";
+}
+
+template <class T>
+ostream& operator << (ostream& os, const set<T>& s) {
+  os << "{";
+  bool first = true;
+  for (const auto& x : s) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << x;
+  }
+  return os << "}";
+}
+
+template <class K, class V>
+ostream& operator << (ostream& os, const map<K, V>& m) {
+  os << "{";
+  bool first = true;
+  for (const auto& kv : m) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << kv.first << ": " << kv.second;
+  }
+  return os << "}";
+}
+
+template<class T, class U>
+void AssertEqual(const T& t, const U& u, const string& hint = {}) {
+  if (t != u) {
+    ostringstream os;
+    os << "Assertion failed: " << t << " != " << u;
+    if (!hint.empty()) {
+       os << " hint: " << hint;
+    }
+    throw runtime_error(os.str());
+  }
+}
+
+void Assert(bool b, const string& hint) {
+  AssertEqual(b, true, hint);
+}
+
+class TestRunner {
+public:
+  template <class TestFunc>
+  void RunTest(TestFunc func, const string& test_name) {
+    try {
+      func();
+      cerr << test_name << " OK" << endl;
+    } catch (exception& e) {
+      ++fail_count;
+      cerr << test_name << " fail: " << e.what() << endl;
+    } catch (...) {
+      ++fail_count;
+      cerr << "Unknown exception caught" << endl;
+    }
+  }
+
+  ~TestRunner() {
+    if (fail_count > 0) {
+      cerr << fail_count << " unit tests failed. Terminate" << endl;
+      exit(1);
+    }
+  }
+
+private:
+  int fail_count = 0;
+};
+
+int GetDistinctRealRootCount(double a, double b, double c) {
+}
+
+void TestSomething() {
+    AssertEqual(2, GetDistinctRealRootCount(1, -26, 120));
+    AssertEqual(1, GetDistinctRealRootCount(9, -12, 4));
+    AssertEqual(0, GetDistinctRealRootCount(1, 1, 1));
+
+    AssertEqual(1, GetDistinctRealRootCount(0, 1, 1));
+    AssertEqual(0, GetDistinctRealRootCount(9, 0, 4));
+    AssertEqual(2, GetDistinctRealRootCount(9, 4, 0));
+
+    AssertEqual(0, GetDistinctRealRootCount(0, 0, 4));
+    AssertEqual(1, GetDistinctRealRootCount(4, 0, 0));
+};
+
+void TestAll() {
+    TestRunner runner;
+    runner.RunTest(TestSomething,"TestSomething");
+};
+
+
+int main() {
+  TestAll();
+  // добавьте сюда свои тесты
+  return 0;
+}
